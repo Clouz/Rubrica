ad7600f5905c93d0204830affa0cbcf8fa2ee36d
Clouz <clouz85@gmail.com>
2019-09-28 18:26:10 +0200

    read data from json file

--- Agenda/MainWindow.xaml.cs
+++ Agenda/MainWindow.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.IO;
 
 namespace Agenda
 {
@@ -25,10 +28,14 @@ namespace Agenda
 
         public MainWindow()
         {
-            List<User> users = new List<User>();
-            users.Add(new User() { Id = 1, Name = "John Doe", Department = "ELE", Phone = "00593" });
-            users.Add(new User() { Id = 2, Name = "john doe", Department = "STRUM", Phone = "00594" });
-            users.Add(new User() { Id = 3, Name = "Banana", Department = "ELE", Phone = "00595" });
+
+            string text = File.ReadAllText(@"Rubrica.json", Encoding.UTF8);
+            
+
+            List<User> users = Deserialize(text);
+            //users.Add(new User() { Name = "John Doe", Department = "ELE", Phone = "00593" });
+            //users.Add(new User() { Name = "john doe", Department = "STRUM", Phone = "00594" });
+            //users.Add(new User() { Name = "Banana", Department = "ELE", Phone = "00595" });
 
 
             InitializeComponent();
@@ -55,17 +62,34 @@ namespace Agenda
                 return u.Name.Contains(SearchBar.Text, comp) || u.Phone.Contains(SearchBar.Text, comp) || u.Department.Contains(SearchBar.Text, comp);
             };
         }
+
+        public class User
+        {
+            public string Name { get; set; }
+            public string Department { get; set; }
+            public string Phone { get; set; }
+        }
+
+        string Serialize(User value)
+        {
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true
+            };
+
+            return JsonSerializer.Serialize<User>(value, options);
+        }
+
+        List<User> Deserialize(string json)
+        {
+            var options = new JsonSerializerOptions
+            {
+                AllowTrailingCommas = true
+            };
+
+            return JsonSerializer.Deserialize<List<User>>(json, options);
+        }
     }
 
-    public class User
-    {
-        public int Id { get; set; }
 
-        public string Name { get; set; }
-
-        public string Department { get; set; }
-
-        public string Phone { get; set; }
-
-    }
 }
